cd0d125010a59749ec316fd1040d99e6bb5098ee
zhejia14 <106137353+zhejia14@users.noreply.github.com>
2023-06-02 17:00:41 +0800

    Create Array_1_q45.cpp

---
+++ Array_1_q45.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+using namespace std;
+
+
+int main() {
+   string str;
+   getline(cin, str, '\n');
+   cout<<"There are "<<str.length()<<" characters"<<endl; 
+    return 0;
+}
